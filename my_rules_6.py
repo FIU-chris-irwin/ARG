e2b45f19c51d78021c097621d22a483b52a6c5e2
Christopher Irwin <cirwi005@fiu.edu>
2023-10-01 10:56:27 -0400

    added argument parser

--- my_rules_6.py
+++ my_rules_6.py
@@ -5,3 +5,26 @@
 # Create functions to generate output files
 # Write code needed for plots
 # Initial Report & Current Draft
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Association Rule Generator")
+    parser.add_argument("--minsup", required=True, help="minimum support count")
+    parser.add_argument("--minconf", required=True, help="minimum confidence")
+    parser.add_argument("--input", required=True, help="input file name")
+    parser.add_argument("--output", required=True, help="output file name")
+
+    args = parser.parse_args()
+
+    output_file = args.output
+    if args.minconf == -1:
+        print('Minconf is -1;exiting without writing file')
+        exit()
+        
+    
+    with open(output_file, 'w') as f:
+        f.write(f"output test\n")
+
+if __name__ == "__main__":
+    main()
