bd0ac3a16d2897803726fdd4937491e6ca6f7779
Christopher Irwin <cirwi005@fiu.edu>
2023-10-03 21:57:22 -0400

    added write_output(). generates 2/3 required files

--- my_rules_6.py
+++ my_rules_6.py
@@ -40,20 +40,44 @@ def read_input(input_file):
 
 def generate_f1(item_counts, minsup):
     """
-    takes a dictionary of items with support counts, and a minimum support count;  returns a sorted list of items that meet minsup 
+    takes a dictionary of items with support counts, and a minimum support count;  returns a sorted dictionary of items that meet minsup 
     """
-    f1_items = []
+    f1_items = {}
     for item, count in item_counts.items():
         if count >= minsup:
-            f1_items.append(item)
-    f1_items.sort()
+            f1_items[item] = count
+    
+    return dict(sorted(f1_items.items()))
 
-    return f1_items
-
-def write_output(output_file):
+def write_output(f1_items, minsup, minconf, input_file, output, item_counts, transactions):
     """
     writes three different output files that describe various aspects of the association rules
     """
+    f1_items, minsup, minconf, input_file, output, item_counts, transactions = f1_items, minsup, minconf, input_file, output, item_counts, transactions
+    
+    # write {output}_items_6.txt
+    with open(f'{output}_items_6.txt', 'w') as items:
+        for itemset, count in f1_items.items():
+            items.write(f'item{itemset}|{count}\n')
+    
+    # write {output}_rules_6.txt
+    if minconf != -1:
+        pass
+
+    # write {output}_info_6.txt
+    
+    with open(f'{output}_info_6.txt', 'w') as info:
+        info.write(f"minsup: {minsup} \n")
+        info.write(f"minconf: {minconf} \n")
+        info.write(f"input file: {input_file} \n")
+        info.write(f"output name: {output} \n")
+        info.write(f"number of items: {len(item_counts)}\n")
+        info.write(f"number of transactions: {len(transactions)}\n")
+        for transaction, list in transactions.items():
+             info.write(f"Transaction {transaction}: {list}\n")
+        for item, count in item_counts.items():
+             info.write(f"Item {item}: {count} times\n")
+    
 
 def main():
     # Get the arguments from terminal and assign to variables
@@ -68,7 +92,7 @@ def main():
     minsup = int(args.minsup)
     minconf = float(args.minconf)
     input_file = args.input
-    output_file = args.output
+    output= args.output
     
     # Do not generate rules when minconf = -1
     if minconf == -1:
@@ -78,20 +102,8 @@ def main():
     item_counts, transactions = read_input(input_file)
 
     f1_items = generate_f1(item_counts, minsup)
-    print(f1_items)
 
-    # FOR TESTING - write output file 
-    with open(output_file, 'w') as output:
-        output.write(f"minsup: {minsup} \n")
-        output.write(f"minconf: {minconf} \n")
-        output.write(f"input file: {input_file} \n")
-        output.write(f"output name: {output_file} \n")
-        output.write(f"number of items: {len(item_counts)}\n")
-        output.write(f"number of transactions: {len(transactions)}\n")
-        for transaction, list in transactions.items():
-             output.write(f"Transaction {transaction}: {list}\n")
-        for item, count in item_counts.items():
-             output.write(f"Item {item}: {count} times\n")
+    write_output(f1_items, minsup, minconf, input_file, output, item_counts, transactions)
 
 if __name__ == "__main__":
     main()
